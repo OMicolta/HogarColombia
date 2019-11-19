5647864f2ddb8d8eb1c22da705a23f1c462ed73f
OMicolta <oivar.1701818060@ucaldas.edu.co>
2019-11-19 14:56:05 -0500

    Componentes del portal web con formulario de login y las respectivas validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { HomeComponent } from './public/home/home.component';
+import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
